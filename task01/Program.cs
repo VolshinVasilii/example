0a706125314614fc0cd1f6738894da7cd03abf23
Vasilii Volshin <vasek9508@gmail.com>
2022-12-15 22:42:02 +0400

    task01

---
+++ task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if (numberTwo * numberTwo == numberOne) 
+{
+    Console.WriteLine($"{numberOne} является квадратом {numberTwo}");
+}
+else
+{
+    Console.WriteLine($"{numberOne} не является квадратом {numberTwo}");
+}
