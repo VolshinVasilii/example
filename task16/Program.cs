cef966f1cc295a7d35c62e06c0fcb5824c6da154
Vasilii Volshin <vasek9508@gmail.com>
2022-12-19 23:50:53 +0400

    task16

---
+++ task16/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SquareNum(number1, number2) ? "Да" : "Нет");
+
+bool SquareNum(int num1, int num2)
+{
+    return number1 == number2 * number2 || number2 == number1 * number1;
+}
