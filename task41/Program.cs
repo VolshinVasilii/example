625648f227eebb59bdede015904f2b6d94e7180d
Vasilii Volshin <vasek9508@gmail.com>
2023-01-25 23:24:16 +0400

    tasks 41, 43

---
+++ task41/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] CreateArray(int arr)
+{
+    int[] array = new int[arr];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите следующее число");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int NumberOfPositiveDigits(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите количество вводимых цифр");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+    Console.WriteLine("Вы ввели неверное количество вводимых цифр, попробуйте заново");
+else
+{
+int[] array = CreateArray(number);
+int numOfPosDigits = NumberOfPositiveDigits(array);
+PrintArray(array);
+Console.WriteLine($"Количество положительных введенных элементов: {numOfPosDigits}");
+}
---
+++ task43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double FindX(double a, double b, double c, double d)
+{
+    double x = (c - a) / (b - d);
+    return x;
+}
+
+double FindY(double a, double b, double c, double d)
+{
+    double x = FindX(a, b, c, d);
+    double y = d * x + c;
+    return y;
+}
+
+Console.WriteLine("Введите координаты точек k1, b1, k2, b2");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+double x = FindX(b1, k1, b2, k2);
+double y = FindY(b1, k1, b2, k2);
+if (k1 == k2) Console.WriteLine("Прямые параллельны");
+else
+{
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}, {y})");
+}
