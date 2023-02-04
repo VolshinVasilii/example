af7ab91d5c2440e85538da3ab951b2a455284bbf
Vasilii Volshin <vasek9508@gmail.com>
2023-02-04 21:49:35 +0400

    tasks 54, 56, 58

---
+++ task56/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей 
+// суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+// с наименьшей суммой элементов: 1 строка 
+
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int NumOfRowsMinimumSum(int[,] matrix)
+{
+    int result = 0;
+    int[] arr = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            arr[i] += matrix[i, j];
+        }
+    }
+    int minNumber = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (minNumber > arr[i])
+        {
+            minNumber = arr[i];
+            result = i+1;
+        }
+    }
+    return result;
+}
+
+int[,] array = CreateMatrixRndInt(4, 4, 1, 4);
+PrintMatrix(array);
+int numOfRow = NumOfRowsMinimumSum(array);
+Console.WriteLine($"Строка с минимальной суммой элементов -> {numOfRow}");
---
+++ task58/Program.cs
@@ -0,0 +1,56 @@
+﻿int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void MatrixProduct(int[,] firstMatrix, int[,] secondMatrix, int[,] productMatrix)
+{
+    for (int i = 0; i < productMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < productMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMatrix.GetLength(1); k++)
+                sum += firstMatrix[i, k] * secondMatrix[k, j];
+            productMatrix[i, j] = sum;
+        }
+    }
+}
+
+int[,] firstArray = CreateMatrixRndInt(3, 3, 1, 3);
+PrintMatrix(firstArray);
+Console.WriteLine();
+int[,] secondArray = CreateMatrixRndInt(2, 3, 1, 3);
+PrintMatrix(secondArray);
+Console.WriteLine();
+int row = firstArray.GetLength(0);
+int col = secondArray.GetLength(1);
+if (row == col)
+{
+    int[,] result = new int[row, col];
+    MatrixProduct(firstArray, secondArray, result);
+    Console.WriteLine("Результат умножения матриц:");
+    PrintMatrix(result);
+}
+else Console.WriteLine("Число строк первой матрицы не соответствует числу столбцов второй матрицы - перемножение невозможно");
+
